042cededb0134e01dd81507aa59a6c94c2ab1969
Moatess <joaompmf.09@gmail.com>
2023-12-22 00:06:00 +0000

    Inicio do frontend

--- src/frontend/Programa.java
+++ src/frontend/Programa.java
@@ -1,6 +1,9 @@
 package frontend;
 
+import backend.Professor;
 import backend.Sistema;
+import java.time.LocalDate;
+
 
 public class Programa {
 
@@ -18,7 +21,7 @@ public class Programa {
             opcao = programa.consola.lerInteiro(opcoes);
             switch (opcao) {
                 case 1:
-
+                    programa.criarProfessor();
                     break;
                 case 2:
 
@@ -27,5 +30,26 @@ public class Programa {
 
         } while (opcao != opcoes.length);
 
+    }
+
+    public void criarProfessor() {
+        System.out.println("===== Adicionar Professor =====");
+
+        String username = consola.lerString("Username do Professor: ");
+        String password = consola.lerString("Password do Professor: ");
+        String nome = consola.lerString("Nome do Professor: ");
+        String numeroMecanografico = consola.lerString("Número Mecanográfico do Professor: ");
+
+        String startDateString = consola.lerString("Data de Início de Funções do Professor (YYYY-MM-DD): ");
+        LocalDate inicioFuncoes = LocalDate.parse(startDateString);
+
+        // Aqui você pode continuar pedindo outras informações necessárias para criar um professor
+        // Agora, crie uma instância de Professor com as informações fornecidas
+        Professor professor = new Professor(username, password, nome, numeroMecanografico, inicioFuncoes, null);
+
+        // Adicione o professor ao RepositorioProfessores
+        sistema.professores.inserirProfessor(username, professor);
+
+        System.out.println("Professor adicionado com sucesso!");
     }
 }
